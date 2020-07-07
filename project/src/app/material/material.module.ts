550adfb5c85e5d0c1f90afb840232bc789d5f53f
Ranjani <ranjanisingh312000@gmail.com>
2020-07-08 00:11:03 +0530

    added angular material

---
+++ project/src/app/material/material.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatSelectModule } from "@angular/material/select";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatInputModule } from "@angular/material/input";
+
+const material = [MatToolbarModule, MatIconModule, MatTabsModule, MatButtonModule, MatMenuModule, MatGridListModule, MatDividerModule, MatProgressBarModule, MatSelectModule, MatCheckboxModule, MatInputModule];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [material],
+})
+export class MaterialModule { }
